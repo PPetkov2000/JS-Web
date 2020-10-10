8c51f37c679092d5f1629d7a20083f22d0ca326c
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-10-10 22:33:31 +0300

    add utils

---
+++ JS-Backend/Cat Shelter/utils/isAuth.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+const { cookie, secret } = require("../config/config").development;
+const User = require("../models/User");
+
+module.exports = (justContinue = false) => {
+  return function (req, res, next) {
+    const token = req.cookies[cookie] || "";
+
+    jwt.verify(token, secret, (err, decodedToken) => {
+      if (err) {
+        if (justContinue) {
+          next();
+          return;
+        }
+        return res.redirect("/user/login");
+      }
+
+      User.findById(decodedToken.userId).then((user) => {
+        req.user = user;
+        next();
+      });
+    });
+  };
+};
