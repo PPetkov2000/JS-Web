45c3a40f569f2b19d9dcfaa6d7f2049aa37d7469
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-10-13 23:47:31 +0300

    Update express.js

--- JS-Backend/Cat Shelter/config/express.js
+++ JS-Backend/Cat Shelter/config/express.js
@@ -1,24 +1,21 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
-const cookieParser = require("cookie-parser");
 const methodOverride = require("method-override");
-const { select } = require("../helpers/hbs");
 
 module.exports = (app) => {
   app.engine(
     "hbs",
     handlebars({
-      helpers: { select },
       layoutsDir: "views",
       defaultLayout: "base-layout",
       partialsDir: "views/partials",
       extname: "hbs",
     })
   );
+
   app.set("view engine", "hbs");
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
-  app.use(express.static("static"));
-  app.use(cookieParser());
+  app.use(express.static("public"));
   app.use(methodOverride("_method"));
 };
