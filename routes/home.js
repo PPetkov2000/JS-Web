461a8c4779f1c9e719a9e12e3443c516c1482368
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-10-15 19:01:52 +0300

    Delete home.js

--- routes/home.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const controllers = require("../controllers/index");
-
-router.get("/", controllers.home.get.listAllCats);
-router.get("/search", controllers.home.get.searchCat);
-
-module.exports = router;
