bc2938ff0ba5f95bd605bfa9e3c30edb08334842
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-10-18 22:25:26 +0300

    Update Post.js

--- JS-Backend/REST API/models/Post.js
+++ JS-Backend/REST API/models/Post.js
@@ -1,1 +1,21 @@
-Post Model
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    minlength: [5, "Post title should be atleast 5 characters long."],
+    required: [true, "Title is required."],
+  },
+  description: {
+    type: String,
+    minlength: [10, "Post description should be atleast 10 characters long."],
+    required: [true, "Description is required."],
+  },
+  creatorId: {
+    type: "ObjectId",
+    ref: "User",
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Post", postSchema);
