1f8168dda8561df5e8bc6adc52989b8fdd644c4e
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-10-18 22:25:42 +0300

    Update User.js

--- JS-Backend/REST API/models/User.js
+++ JS-Backend/REST API/models/User.js
@@ -1,1 +1,36 @@
-User Model
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "Please enter a username."],
+  },
+  password: {
+    type: String,
+    required: [true, "Please enter a password."],
+  },
+  posts: [
+    {
+      type: "ObjectId",
+      ref: "Post",
+    },
+  ],
+});
+
+userSchema.methods.passwordsMatch = async function (password) {
+  const match = await bcrypt.compare(password, this.password);
+  return match;
+};
+
+userSchema.pre("save", async function (next) {
+  try {
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password = hash;
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = mongoose.model("User", userSchema);
