242c59d58bd8013b287baf55c79a009ba2fb2241
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-10-19 03:41:46 +0300

    Update database.js

--- JS-Backend/REST API/config/database.js
+++ JS-Backend/REST API/config/database.js
@@ -1,1 +1,10 @@
-database;
+const mongoose = require("mongoose");
+const { dbUrl } = require("./config").development;
+
+module.exports = () => {
+  return mongoose.connect(
+    dbUrl,
+    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
+    console.log("Database is setup and running!")
+  );
+};
