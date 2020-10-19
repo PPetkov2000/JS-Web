e796757c79184d461de2292001b65c4cbcabadf2
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-10-19 03:40:45 +0300

    Update config.js

--- JS-Backend/REST API/config/config.js
+++ JS-Backend/REST API/config/config.js
@@ -1,1 +1,11 @@
-config;
+module.exports = {
+  development: {
+    port: process.env.PORT || 8000,
+    dbUrl:
+      process.env.DB_URL ||
+      "mongodb+srv://user:softuni-password@softuni.cpuao.mongodb.net/REST_API?retryWrites=true&w=majority",
+    secret: process.env.JWT_SECRET || "my_secret",
+    cookie: process.env.JWT_AUTH_COOKIE || "x-auth-token",
+  },
+  production: {},
+};
