88846473457ab362d8134cdcecd3697b45833fe6
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-10-21 23:20:43 +0300

    Update index.js

--- JS-Backend/REST API/index.js
+++ JS-Backend/REST API/index.js
@@ -7,7 +7,7 @@ const app = express();
 const { port } = require("./config/config").development;
 const usersRoutes = require("./routes/users");
 const postsRoutes = require("./routes/posts");
-const errorHandler = require("./utils/errorHandler");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -27,20 +27,21 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((error, req, res, next) => {
-  if (error.message.includes("Cast to ObjectId failed")) {
-    return res.status(404).json({ message: "Post not found!" });
-  }
-  if (error.message.includes("Post validation failed")) {
-    const errors = errorHandler(error);
-    return res.status(400).json({ errors });
-  }
-  // const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+app.use(notFound);
+app.use(errorHandler);
 
-  const status = error.statusCode || 500;
-  const message = error.message;
-  res.status(status).json({ message });
-  next();
-});
+// app.use((error, req, res, next) => {
+//   if (error.message.includes("Cast to ObjectId failed")) {
+//     return res.status(404).json({ message: "Post not found!" });
+//   }
+//   if (error.message.includes("Post validation failed")) {
+//     const errors = errorHandler(error);
+//     return res.status(400).json({ errors });
+//   }
+//   console.log(req.originalUrl);
+//   console.log(error);
+//   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+//   res.status(statusCode).json({ message: error.message });
+// });
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
