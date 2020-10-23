95f2abea3276f6f3bc217b9e82f578d3ecad0ea0
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-10-23 19:32:50 +0300

    add controller factory

---
+++ JS-Backend/REST API/modules/controller-factory.js
@@ -0,0 +1,96 @@
+module.exports = function (model) {
+  function getAll(req, res, next) {
+    model
+      .find()
+      .then((docs) => {
+        return res.json(docs);
+      })
+      .catch(next);
+  }
+
+  function getOne(req, res, next) {
+    const id = req.params.id;
+    model
+      .findById(id)
+      .then((doc) => {
+        return res.json(doc);
+      })
+      .catch(next);
+  }
+
+  function createOne(req, res, next) {
+    model
+      .create(req.body)
+      .then((doc) => {
+        return res.status(201).json(doc);
+      })
+      .catch(next);
+  }
+
+  function updateOne(req, res, next) {
+    const id = req.params.id;
+    model
+      .updateOne({ _id: id }, req.body)
+      .then(() => {
+        return res.status(200).json({ message: "Updated successfully!" });
+      })
+      .catch(next);
+  }
+
+  function deleteOne(req, res, next) {
+    const id = req.params.id;
+    model
+      .findOneAndDelete({ _id: id })
+      .then((post) => {
+        return res.json({ message: "Deleted successfully!" });
+      })
+      .catch(next);
+  }
+
+  function createOneWithRelations(relationModel, relationDoc) {
+    return function (req, res, next) {
+      model
+        .create({ ...req.body, creatorId: req.user._id })
+        .then((createdDoc) => {
+          return Promise.all([
+            createdDoc,
+            relationModel.updateOne(
+              { _id: req.user._id },
+              { $push: { [relationDoc]: createdDoc._id } }
+            ),
+          ]);
+        })
+        .then(([createdDoc, _]) => {
+          return res.status(201).json(createdDoc);
+        })
+        .catch(next);
+    };
+  }
+
+  function deleteOneWithRelations(relationModel, relationDoc) {
+    return function (req, res, next) {
+      model
+        .deleteOne({ _id: req.params.id })
+        .then(() => {
+          return relationModel.updateOne(
+            { _id: req.user._id },
+            { $pull: { [relationDoc]: req.params.id } }
+          );
+        })
+        .then(() => {
+          return res.json({ message: "Deleted successfully!" });
+        })
+        .catch(next);
+    };
+  }
+
+  return {
+    getAll,
+    getOne,
+    createOne,
+    updateOne,
+    deleteOne,
+    createOneWithRelations,
+    deleteOneWithRelations,
+  };
+};
