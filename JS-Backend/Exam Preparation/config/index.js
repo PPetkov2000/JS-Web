24191b8ee081e595b334d7e715effd9bbc65fe0c
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-10-26 22:12:48 +0200

    Update index.js

--- JS-Backend/Exam Preparation/config/index.js
+++ JS-Backend/Exam Preparation/config/index.js
@@ -1,1 +1,15 @@
-init;
+const env = process.env.NODE.ENV || "development";
+
+const config = {
+  development: {
+    port: process.env.PORT || 4000,
+    dbUrl:
+      "mongodb+srv://user:softuni-password@softuni.cpuao.mongodb.net/Shoes?retryWrites=true&w=majority",
+    cookie: "x-auth-token",
+    secret: "SuperDuperSecret",
+    saltRounds: 10,
+  },
+};
+
+module.exports = config[env];
+
