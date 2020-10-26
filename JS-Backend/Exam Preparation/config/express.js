74b669a5e93258f47920e58148c6989285479cc7
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-10-26 22:13:19 +0200

    express config

---
+++ JS-Backend/Exam Preparation/config/express.js
@@ -0,0 +1,24 @@
+const handlebars = require("express-handlebars");
+const cookieParser = require("cookie-parser");
+const auth = require("../utils/auth");
+
+module.exports = (express, app) => {
+  app.use(express.static("public"));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+
+  app.use(cookieParser());
+
+  app.use(auth);
+
+  app.engine(
+    "hbs",
+    handlebars({
+      layoutsDir: "views",
+      defaultLayout: "base-layout.hbs",
+      partialsDir: "views/partials",
+      extname: "hbs",
+    })
+  );
+  app.set("view engine", "hbs");
+};
