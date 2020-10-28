2513ce5b8b9840e958ffdf2c0b6ae874d38af153
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-10-28 22:48:44 +0200

    Update index.js

--- JS-Backend/Exam Preparation/models/index.js
+++ JS-Backend/Exam Preparation/models/index.js
@@ -1,1 +1,10 @@
-index;
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const getUserModel = require('./User');
+const getShoeModel = require('./Shoe');
+
+module.exports = {
+    User: getUserModel(mongoose, bcrypt),
+    Shoe: getShoeModel(mongoose)
+};
