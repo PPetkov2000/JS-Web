9f10f3641912228af781b16ee17f23784ace9b4a
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-10-28 22:52:03 +0200

    Update jwt.js

--- JS-Backend/Exam Preparation/utils/jwt.js
+++ JS-Backend/Exam Preparation/utils/jwt.js
@@ -1,1 +1,22 @@
-jwt;
+const jwt = require('jsonwebtoken');
+const { secret } = require('../config');
+
+module.exports = {
+
+    createToken(_id) {
+        return jwt.sign({ _id }, secret, { expiresIn: '1h' })
+    },
+
+    verifyToken(token) {
+
+        return new Promise((resolve, reject) => {
+            jwt.verify(token, secret, (err, payload) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve(payload);
+            })
+        })
+    }
+};
