ac5a531c483a3bc22bf20dda38cb6cb7b9cb82fe
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-10-31 22:10:21 +0200

    Update index.js

--- JS-Backend/Exam Preparation/controllers/index.js
+++ JS-Backend/Exam Preparation/controllers/index.js
@@ -1,1 +1,9 @@
-index;
+const homeController = require("./homeController");
+const userController = require("./userController");
+const shoesController = require("./shoesController");
+
+module.exports = {
+  homeController,
+  userController,
+  shoesController,
+};
