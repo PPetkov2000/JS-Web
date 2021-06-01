df80dd8af05698005f2b1a22d2b17e53dbb833fa
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-06-01 20:31:45 +0300

    Update auth.js

--- JS-Backend/REST API/middleware/auth.js
+++ JS-Backend/REST API/middleware/auth.js
@@ -1,8 +1,8 @@
-const jwt = require("jsonwebtoken");
-const { secret } = require("../config/config").development;
+const { cookie } = require("../config/config").development;
 const User = require("../models/User");
+const createToken = require("../utils/createToken");
 
-async function validateUserData(req, res, next) {
+async function signup(req, res, next) {
   const { username, password, repeatPassword } = req.body;
 
   try {
@@ -18,7 +18,20 @@ async function validateUserData(req, res, next) {
       throw new Error("This username is already taken.");
     }
 
-    next();
+    const user = await User.create({ username, password });
+
+    if (!user) {
+      res.status(400);
+      throw new Error("Invalid user data.");
+    }
+
+    const token = createToken(user);
+
+    return res.cookie(cookie, token, { maxAge: 3600000 }).status(200).json({
+      message: "User successfully logged in!",
+      userId: user._id.toString(),
+      token,
+    });
   } catch (error) {
     next(error);
   }
@@ -35,25 +48,27 @@ async function authenticate(req, res, next) {
       throw new Error("Invalid email or password.");
     }
 
-    const token = jwt.sign(
-      { userId: user._id.toString(), username: user.username },
-      secret,
-      { expiresIn: "1h" }
-    );
+    const token = createToken(user);
 
-    return res
-      .cookie("auth-token", token, { maxAge: 3600000 })
-      .status(200)
-      .json({
-        message: "User successfully logged in!",
-        userId: user._id.toString(),
-      });
+    return res.cookie(cookie, token, { maxAge: 3600000 }).status(200).json({
+      message: "User successfully logged in!",
+      userId: user._id.toString(),
+      token,
+    });
   } catch (error) {
     next(error);
   }
 }
 
+function logout(req, res) {
+  req.user = null;
+  req.session.destroy((err) => {
+    res.clearCookie(cookie, { path: "/" });
+  });
+}
+
 module.exports = {
   authenticate,
-  validateUserData,
+  signup,
+  logout,
 };
